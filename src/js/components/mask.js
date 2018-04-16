8ba6f01cedb3cb3d35dcaac1a8f0d81dba55d1f1
Friedrich <hello@sym.design>
2018-04-16 17:58:00 +0200

    mask.js regex string parsing

--- src/js/components/mask.js
+++ src/js/components/mask.js
@@ -14,8 +14,7 @@ export var Mask = function( wakkle ) {
         masked      = [];
 
     this.init = function() {
-
-        console.log(prefix)
+        
         for ( var i = 0; i < wakkle.markup.length; i++ ) {
 
             markup = wakkle.markup[i];
@@ -29,53 +28,60 @@ export var Mask = function( wakkle ) {
                 xhr.url = wakkle.markup[i].getAttribute('mask');
                 xhr.open('GET', xhr.url, true);
                 xhr.send();
+                
 
                 xhr.onload = function(e) {
-
+                    
                     if ( xhr.status >= 200 && xhr.status < 400 ) {
 
                         var container       = document.createElement('div');
-                        container.innerHTML = xhr.responseText;
+                        var responseText    = xhr.responseText;
 
-                        wakkle.wrapper.appendChild( container );
                         // https://stackoverflow.com/a/22277907
 
                         var svg         = container.getElementsByTagName('svg')[0],
-                            viewBox     = svg.getAttribute('viewBox').replace(/^\s+|\s+$/gm,'').split(' '),
-                            clipPaths   = container.getElementsByTagName('clipPath'),
-                            clipPath,
+                            viewBox     = responseText.match(/ viewBox="([^"]*)"/i)[1].replace(/^\s+|\s+$/gm,'').split(' '),
                             markup      = wakkle.wrapper.querySelector('[mask="' + xhr.url + '"]'),
                             id          = markup.id || generateUUID();
 
                         markup.id = markup.id || id;
-                        
-                        // Remove some of the attributes that aren't needed
-                        svg.removeAttribute('xmlns:a');
-                        svg.removeAttribute('width');
-                        svg.removeAttribute('height');
-                        svg.removeAttribute('x');
-                        svg.removeAttribute('y');
-                        svg.removeAttribute('enable-background');
-                        svg.removeAttribute('xmlns:xlink');
-                        svg.removeAttribute('xml:space');
-                        svg.removeAttribute('version');
 
-                        svg.style.top = '-100%';
+                        // Make CSS clip-path responsive
+                        // add transform="scale(' + ( 1 / viewBox[2]) + ')" clipPathUnits="objectBoundingBox"
 
-                        for ( var i = 0; i < clipPaths.length; i++ ) {
-                            clipPath = clipPaths[i];
-
-                            clipPath.setAttributeNS('http://www.w3.org/2000/svg','clipPathUnits','objectBoundingBox');
-                            clipPath.setAttributeNS('http://www.w3.org/2000/svg','transform','scale(' + (1/viewBox[2]) + ','+ (1/viewBox[3]) +')');
-                            // http://meyerweb.com/eric/thoughts/2017/02/24/scaling-svg-clipping-paths-for-css-use/
-
-                            clipPath.id = id + '--' + pad( ( clipPath.id ? clipPath.id : i ), wakkle.sequence.padding );
-                            
+                        if ( !responseText.includes('transform') ) {
+                            responseText = responseText.replace( /<clipPath([^>]*)>/gi, function( match, $1) {
+                                return '<clipPath' + $1 + ' transform="scale(' + ( 1 / viewBox[2]) + ',' + ( 1 / viewBox[3]) + ')">'
+                            })
                         }
 
-                        wakkle.wrapper.appendChild( svg );
+                        if ( !responseText.includes('clipPathUnits') || !responseText.includes('objectBoundingBox') ) {
+                            responseText = responseText.includes('userSpaceOnUse') ? responseText.replace( /clipPathUnits=["']userSpaceOnUse["']/gi, '' ) : responseText;
+                            responseText = responseText.replace( /<clipPath([^>]*)>/gi, function( match, $1) {
+                                return '<clipPath' + $1 + ' clipPathUnits="objectBoundingBox">'
+                            })
+                        }
 
-                        svg.innerHTML = svg.innerHTML
+                        // Remove all attributes apart from viewBox
+                        responseText = responseText.replace( /<svg(?:"[^"]*"['"]*|'[^']*'['"]*|[^'">])+>/i, '<svg viewBox="' + responseText.match(/ viewBox="([^"]*)"/i)[1] + '">' );
+
+                        // Make unique ids for CSS clip-path referencing
+                        var i = 0;
+                        responseText = responseText.replace(/(<clipPath.*id=["'])((?:\\.|[^"\\])*)(["'])/gi, function( match, $1, $2, $3) {
+
+                            var id = markup.id || generateUUID();
+
+                            i++;
+                            $2 = id + '--' + pad( ( $2 ? $2 : i ), wakkle.sequence.padding )
+                            
+                            return $1 + $2 + $3;
+
+                        })
+                        console.log( responseText )
+
+                        container.innerHTML = responseText;
+                        container.style.position = 'absolute';
+                        document.body.insertBefore( container, document.body.firstChild );
 
                     }
                 }
